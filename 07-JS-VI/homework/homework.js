bcf7097f9d4e6fc3e57c6956ea1381927fc8abb6
thegera4 <84020433+thegera4@users.noreply.github.com>
2022-01-28 15:54:18 -0600

    tarea6vf

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -53,10 +53,10 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
-  var nuevoArray = []
-  for(var i = 0; i < array.length; i++){
-    nuevoArray.push(cb(array[i]));
-  }
+  var nuevoArray = array.map(function(elemento) {
+    return cb(elemento);
+  });
+  return nuevoArray;
 }
 
 function filter(array) {
